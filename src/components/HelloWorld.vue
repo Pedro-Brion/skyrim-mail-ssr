896d3c78a64e22c2de78b6e089bbb7b2ce722d7f
Pedro Brion <pedro.brion@gmail.com>
2025-07-08 19:26:46 -0300

    adding a logo to test asset handling

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -21,12 +21,17 @@ async function refetch() {
 
 <template>
   <div class="flex flex-col place-items-center">
-    <h1 class="text-3xl">Mail Skyrim??</h1>
+    <img class="w-28" src="/logo.webp" alt="" />
+    <h1 class="text-3xl">Skyrim Mail</h1>
     <MainButton @click="refetch"> Refetch </MainButton>
   </div>
-  <h2 v-if="loading">Loading.....</h2>
-  <div class="flex flex-col gap-2 w-[75vw] mx-auto mt-4" v-else>
-   <MessageItem v-for="message of messages" :key="message.id" :message></MessageItem>
+  <h2 class="text-center" v-if="loading">Loading.....</h2>
+  <div class="flex flex-col gap-2 w-[75vw] mx-auto mt-4">
+    <MessageItem
+      v-for="message of messages"
+      :key="message.id"
+      :message
+    ></MessageItem>
   </div>
 </template>
 
